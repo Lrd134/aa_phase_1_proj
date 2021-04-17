9943ec71c3d1d2c34ae61fed9bca44a9f5a9ba72
Lrd134 <larryc3200@gmail.com>
2021-04-17 14:02:12 -0400

    fixed opening error

--- lib/newegg_scraper_chsbr/scraper.rb
+++ lib/newegg_scraper_chsbr/scraper.rb
@@ -4,7 +4,7 @@ class NeweggScraperChsbr::Scraper
     
     URL = "https://www.newegg.com/p/pl?d=cpu"
     def initialize(url = URL)
-        @xml_obj = Nokogiri::HTML(open(url))  # This opens the URL with open-uri, then parses it as an XML Object
+        @xml_obj = Nokogiri::HTML(URI.open(url))  # This opens the URL with open-uri, then parses it as an XML Object
     end
     
 
